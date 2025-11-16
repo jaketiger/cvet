fe86b4c5d7bfb887913aa4660e16f4dfa849e8ef
jaketiger <kurb@yandex.ru>
2025-11-16 21:47:41 +0300

    ошибки для мобильных вер

--- megacvet_project/settings.py
+++ megacvet_project/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'shop.middleware.AdminViewportMiddleware',
 ]
 
 ROOT_URLCONF = 'megacvet_project.urls'
