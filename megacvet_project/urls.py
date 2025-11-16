1e52e4852f3a9b2fa625b4df4aad32f674e53cc3
jaketiger <kurb@yandex.ru>
2025-11-16 10:46:05 +0300

    Стили для сайта и ошибка для мобильных

--- megacvet_project/urls.py
+++ megacvet_project/urls.py
@@ -18,31 +18,25 @@ urlpatterns = [
          name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 
-    # --- Явные URL'ы для сброса пароля ---
-    # 1. Форма запроса сброса (использует нашу кастомную view)
     path('accounts/password_reset/', user_views.password_reset_request, name='password_reset'),
-
-    # 2. Сообщение "Проверьте почту" (использует стандартную view)
     path('accounts/password_reset/done/',
          auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'),
          name='password_reset_done'),
-
-    # 3. Ссылка из письма, ведущая на форму ввода нового пароля
     path('accounts/reset/<uidb64>/<token>/',
          auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'),
          name='password_reset_confirm'),
-
-    # 4. Сообщение об успешном сбросе
     path('accounts/reset/done/',
          auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
          name='password_reset_complete'),
 
     # 3. URL-адреса наших приложений
-    path('accounts/', include('users.urls')),  # Для регистрации
+    # Эта строка подключает users/urls.py и видит 'app_name = users'
+    path('accounts/', include('users.urls')),
+
     path('cart/', include('cart.urls', namespace='cart')),
     path('orders/', include('orders.urls', namespace='orders')),
     path('', include('shop.urls', namespace='shop')),
 ]
 
-if settings.DEBUG:
+if settings.DEBUG is False:  # Используем is False для продакшена
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
